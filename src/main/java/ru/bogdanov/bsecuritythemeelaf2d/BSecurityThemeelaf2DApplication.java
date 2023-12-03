23ee2bc7e3725f83ba77723e40cd1c90edadaaba
Akimosabe <he7seven@gmail.com>
2023-12-03 19:29:33 +0500

    Initial commit

---
+++ src/main/java/ru/bogdanov/bsecuritythemeelaf2d/BSecurityThemeelaf2DApplication.java
@@ -0,0 +1,13 @@
+package ru.bogdanov.bsecuritythemeelaf2d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSecurityThemeelaf2DApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BSecurityThemeelaf2DApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bogdanov/bsecuritythemeelaf2d/BSecurityThemeelaf2DApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bogdanov.bsecuritythemeelaf2d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BSecurityThemeelaf2DApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
